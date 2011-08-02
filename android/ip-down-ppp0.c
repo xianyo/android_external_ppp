a68c0e1a433bfb37e890fa8f38852727e587d630
b02247 <b02247@freescale.com>
2011-08-02 20:28:40 +0800

    ENGR00154182 update copyright
    update copyright
    
    Signed-off-by: b02247 <b02247@freescale.com>

--- android/ip-down-ppp0.c
+++ android/ip-down-ppp0.c
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* Copyright (C) 2010-2011 Freescale Semiconductor,Inc. */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
